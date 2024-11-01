b79ed439a97ae38615637447ad6935e46826fbef
nafisholeh-apv <rachmad.nafisholeh@appvantage.asia>
2024-11-01 12:53:25 +0700

    style: apply missed lint fix

--- src/server/database/collections.ts
+++ src/server/database/collections.ts
@@ -20,5 +20,5 @@ export const getCollections = ({
     userWebCredentials: regular.db.collection<documents.UserWebCredential>('userWebCredentials'),
     settings: encrypted.db.collection<documents.Setting>('settings'),
     externalLinks: regular.db.collection<documents.ExternalLink>('externalLinks'),
-    lives: regular.db.collection<documents.Life>('lives')
+    lives: regular.db.collection<documents.Life>('lives'),
 });
