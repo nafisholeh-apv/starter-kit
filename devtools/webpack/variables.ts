b9daf060fe21ef2cf4e8d1cd37b67cf6105ee5a5
nafisholeh-apv <rachmad.nafisholeh@appvantage.asia>
2024-11-05 10:25:53 +0700

    fix(test): ensure cypress test can run in dev env

--- devtools/webpack/variables.ts
+++ devtools/webpack/variables.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { Configuration } from 'webpack';
 
+export const isBuildIntentTest = process.env.NODE_ENV === 'test';
 export const isBuildIntentDevelopment = process.env.NODE_ENV === 'development';
 export const isBuildIntentProduction = !isBuildIntentDevelopment;
 
@@ -8,9 +9,7 @@ export const rootDirname = path.resolve(__dirname, '../..');
 export const srcDirname = path.join(rootDirname, './src');
 
 export const buildDirname = path.join(rootDirname, process.env.BUILD_DIR || 'build');
-
-export const webpackMode = isBuildIntentProduction ? 'production' : 'development';
-
+export const webpackMode = isBuildIntentTest || isBuildIntentDevelopment ? 'development' : 'production';
 type WebpackCacheMode = Exclude<Configuration['cache'], boolean>['type'];
 
 export const webpackCacheMode: WebpackCacheMode = (process.env.CACHE_MODE as WebpackCacheMode) || 'memory';
